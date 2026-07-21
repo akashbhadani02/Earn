f489ca82ced187046a81024788cd48aefae9741f
techno-one <asbhai14@gmail.com>
2026-07-21 09:43:42 +0530

    allllll

--- server.js
+++ server.js
@@ -3,11 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const http = require("http");
-const { Server } = require("socket.io");
 
 const connectDB = require("./db");
-const User = require("./models/User");
 
 const authRoutes = require("./routes/auth");
 const walletRoutes = require("./routes/wallet");
@@ -15,58 +12,6 @@ const profileRoutes = require("./routes/profile");
 const adminRoutes=require("./routes/admin");
 
 const app = express();
-const server = http.createServer(app);
-
-const io = new Server(server, {
-    cors: {
-        origin: "*"
-    }
-});
-
-app.set("io", io);
-const User = require("./models/User");
-
-io.on("connection", (socket) => {
-
-    console.log("✅ New Socket Connected");
-
-    socket.on("student-online", async (userId) => {
-
-        socket.userId = userId;
-
-        await User.findByIdAndUpdate(userId, {
-            isOnline: true
-        });
-
-        io.emit("online-update", {
-            userId,
-            isOnline: true
-        });
-
-        console.log(userId + " Online");
-
-    });
-
-    socket.on("disconnect", async () => {
-
-        console.log("❌ Disconnect Fired");
-
-        if (!socket.userId) return;
-
-        await User.findByIdAndUpdate(socket.userId, {
-            isOnline: false
-        });
-
-        io.emit("online-update", {
-            userId: socket.userId,
-            isOnline: false
-        });
-
-        console.log(socket.userId + " Offline");
-
-    });
-
-}); 
 
 connectDB();
 
@@ -89,7 +34,7 @@ module.exports = app;
 if (require.main === module) {
     const PORT = process.env.PORT || 5000;
 
-    server.listen(PORT, () => {
-    console.log(`🚀 Server Running on Port ${PORT}`);
-});
+    app.listen(PORT, () => {
+        console.log(`🚀 Server Running on Port ${PORT}`);
+    });
 }
