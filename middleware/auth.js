4cf70c7ff846c80641df00d8fb7cce963c0c3de2
techno-one <asbhai14@gmail.com>
2026-08-02 18:39:27 +0530

    last seen

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
     try {
 
         const token = req.headers.authorization?.split(" ")[1];
@@ -15,6 +16,11 @@ module.exports = (req, res, next) => {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
         req.user = decoded;
+
+        // દરેક Request પર Last Seen Update થશે
+        await User.findByIdAndUpdate(req.user.id, {
+            lastSeen: new Date()
+        });
 
         next();
 
